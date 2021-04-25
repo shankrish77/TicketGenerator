cde9d9c6558707ecab959c223ea2cb8abea30907
shankrish77 <74517850+shankrish77@users.noreply.github.com>
2021-04-24 17:56:15 -0700

    Update TicketGeneratorApplicationTests.java

--- src/test/java/com/caltech/TicketGenerator/TicketGeneratorApplicationTests.java
+++ src/test/java/com/caltech/TicketGenerator/TicketGeneratorApplicationTests.java
@@ -1,6 +1,7 @@
 package com.caltech.TicketGenerator;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
